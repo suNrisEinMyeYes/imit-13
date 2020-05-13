dcc64cacb9ebcff61e94cdb3ab3685f50b39178d
suNrisEinMyeYes <iklown3@gmail.com>
2020-05-13 23:02:20 +0700

    init

---
+++ imit13/imit13/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace imit13
+{
+    public partial class Form1 : Form
+    {
+        Random rnd;
+        List<float> values = new List<float>();
+        int n;
+        int[] Count;
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.ChartAreas[0].AxisX.IntervalOffset = 0;
+            chart1.ChartAreas[0].AxisX.Interval = 0;
+            rnd = new Random();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            float chi = 9.488f;
+            init();
+            if (check())
+            {
+                StatPanel.Visible = true;
+                double A = 0;
+                int k = 0;
+                float M = 0, D = 0, Derror = 0, Merror = 0, Chi_square = 0;
+                for (int i = 0; i < numericUpDown1.Value; i++)
+                {
+                    A = rnd.NextDouble();
+                    k = 0;
+                    foreach (var value in values)
+                    {
+                        A -= value;
+                        if (A <= 0)
+                        {
+                            Count[k]++;
+                            break;
+                        }
+                        k++;
+                    }
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    chart1.Series[0].Points.AddXY(i, Count[i] / numericUpDown1.Value);
+                }
+
+
+                for (int i = 0; i < n; i++)
+                {
+                    M += (Count[i] / (float)numericUpDown1.Value) * (i + 1);
+                    Merror += values[i] * (i + 1);
+
+                    D += (Count[i] / (float)numericUpDown1.Value) * (i + 1) * (i + 1);
+                    Derror += values[i] * (i + 1) * (i + 1);
+
+                    Chi_square += ((Count[i] * Count[i]) / ((float)numericUpDown1.Value * (float)values[i]));
+                }
+                Chi_square -= (float)numericUpDown1.Value;
+                D -= M * M;
+                Derror -= Merror * Merror;
+                VarianceLbl.Text = D.ToString();
+                AverageLbl.Text = M.ToString();
+                ErrAvgLbl.Text = (Math.Abs(M - Merror) / Math.Abs(Merror)).ToString();
+                ErrVarLbl.Text = (Math.Abs(D - Derror) / Math.Abs(Derror)).ToString();
+                Chi1Lbl.Text = Chi_square.ToString();
+                Chi2Lbl.Text = "9.488";
+                if (Chi_square > chi)
+                {
+                    BoolLbl.Text = "true";
+                }
+                else
+                {
+                    BoolLbl.Text = "false";
+                }
+                float Sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    Sum += values[i];
+                }
+                Console.WriteLine((Sum - n * Merror) / Math.Sqrt(n * Derror));
+
+                for (int i = 0; i < n; i++)
+                {
+                    Count[i] = 0;
+                }
+            }
+
+
+
+        }
+        bool check()
+        {
+            var SumToCheck = 0f;
+            foreach (var value in values)
+            {
+                SumToCheck += (float)value;
+            }
+            if (SumToCheck == 1f)
+            {
+                return true;
+            }
+            else
+            {
+                chart1.Visible = false;
+                ErrorLbl.Visible = true;
+                return false;
+            }
+        }
+
+        void init()
+        {
+            ErrorLbl.Visible = false;
+            chart1.Visible = true;
+            chart1.Series[0].Points.Clear();
+            values.Clear();
+            double Sum = 0;
+            float A;
+            n = Int32.Parse(textBox1.Text, CultureInfo.InvariantCulture.NumberFormat);
+            for (int i = 0; i < n-1; i++)
+            {
+                A = (float)(rnd.NextDouble() % (1 - Sum));
+                Console.WriteLine(A);
+                values.Add(A);
+                Sum += A;
+            }
+            values.Add((float)(1 - Sum));
+            Console.WriteLine(values[n-1]);
+
+
+
+
+
+
+            Count = new int[n];
+            
+            
+            
+        }
+    }
+}
